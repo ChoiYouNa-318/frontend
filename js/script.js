0c261c546f0bc46201800c1566b018299c1b64b4
Yuna Choi <younasi0318@cau.ac.kr>
2023-09-27 17:11:55 +0900

    second commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+console.log("first project script");
+openBtn.addEventListener('click', function () {
+    sideBar.classList.remove('hidden');
+  })
+
+closeBtn.addEventListener('click', function () {
+  sideBar.classList.add('hidden');
+})
